f7156cc9faac42d3cd39b6588f6ff6f74e08d4f6
Frey98 <jtomasek@decisiohealth.com>
2022-07-10 17:23:54 -0500

    cleaning up code

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+const postcss = require('postcss');
+const postcssPresetEnv = require('postcss-preset-env');
+
+postcss([
+  postcssPresetEnv()
+]).process("app.css");
+
 document.addEventListener('DOMContentLoaded', () => {
     const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
     const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
